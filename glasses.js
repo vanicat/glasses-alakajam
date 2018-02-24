5000d987ec4f20a18d6e9a428fc1aeed5546efb7
Rémi Vanicat <vanicat@debian.org>
2018-02-24 19:25:14 +0100

    Colision

--- glasses.js
+++ glasses.js
@@ -10,9 +10,12 @@ TheGame.prototype = {
 
   create: function () {
     this.game.stage.backgroundColor = '#736357'
+    this.game.physics.startSystem(Phaser.Physics.ARCADE)
 
     this.map = this.game.add.tilemap('fst-map')
     this.map.addTilesetImage('tilleset', 'tilleset')
+
+    this.map.setCollision(1, true, this.layer)
 
     this.layer = this.map.createLayer('background')
     this.layer.resizeWorld()
@@ -21,6 +24,7 @@ TheGame.prototype = {
 
     this.player = this.game.add.sprite(this.start.x, this.start.y, 'theman')
     this.player.anchor.setTo(0.5, 0.5)
+    this.physics.enable(this.player, Phaser.Physics.ARCADE)
 
     this.camera.follow(this.player)
 
@@ -28,20 +32,21 @@ TheGame.prototype = {
   },
 
   update: function () {
+    this.game.physics.arcade.collide(this.player, this.layer)
+
     var leftStickX = this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X)
     var leftStickY = this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y)
 
-    if (leftStickX) {
-      this.player.x += leftStickX * 10
-    } else {
+    if (!leftStickX) {
       leftStickX = 0
     }
-
-    if (leftStickY) {
-      this.player.y += leftStickY * 10
-    } else {
+    if (!leftStickY) {
       leftStickY = 0
     }
+
+    this.player.body.velocity.x = leftStickX * 100
+    this.player.body.velocity.y = leftStickY * 100
+
     if (leftStickX || leftStickY) {
       this.player.rotation = Math.atan2(leftStickX, -leftStickY)
     }
