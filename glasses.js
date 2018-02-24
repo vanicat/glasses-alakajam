0e5f3f6880440fc4390e11cb023b2729cdb0a309
Rémi Vanicat <vanicat@debian.org>
2018-02-24 16:54:53 +0100

    Renaming sprite to player

--- glasses.js
+++ glasses.js
@@ -10,8 +10,8 @@ TheGame.prototype = {
   create: function () {
     this.game.stage.backgroundColor = '#736357'
 
-    this.sprite = this.game.add.sprite(300, 300, 'theman')
-    this.sprite.anchor.setTo(0.5, 0.5)
+    this.player = this.game.add.sprite(300, 300, 'theman')
+    this.player.anchor.setTo(0.5, 0.5)
 
     this.pad1 = this.game.input.gamepad.pad1
   },
@@ -21,18 +21,18 @@ TheGame.prototype = {
     var leftStickY = this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y)
 
     if (leftStickX) {
-      this.sprite.x += leftStickX * 10
+      this.player.x += leftStickX * 10
     } else {
       leftStickX = 0
     }
 
     if (leftStickY) {
-      this.sprite.y += leftStickY * 10
+      this.player.y += leftStickY * 10
     } else {
       leftStickY = 0
     }
     if (leftStickX || leftStickY) {
-      this.sprite.rotation = Math.atan2(leftStickX, -leftStickY)
+      this.player.rotation = Math.atan2(leftStickX, -leftStickY)
     }
   },
 
