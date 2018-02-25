81d8a43ed95b4b063158d5c675d24509eaec5486
Rémi Vanicat <vanicat@debian.org>
2018-02-25 10:28:52 +0100

    Factorisation of code

--- glasses.js
+++ glasses.js
@@ -25,54 +25,17 @@ TheGame.prototype = {
     this.layer = this.map.createLayer('background')
     this.layer.resizeWorld()
 
-    this.start = this.findObject('start')
-
-    this.player = this.game.add.sprite(this.start.x, this.start.y, 'theman')
-    this.player.anchor.setTo(0.5, 0.5)
-    this.physics.enable(this.player, Phaser.Physics.ARCADE)
-
-    this.camera.follow(this.player)
+    this.createPlayer()
 
     this.pad1 = this.game.input.gamepad.pad1
 
-    this.blur = this.game.add.filter('MyBlur')
-
-    this.blur.blur = 20
-    this.blur.setResolution(this.game.width, this.game.height)
-    this.blur.centerx = this.game.width / 2
-    this.blur.centery = this.game.height / 2
-
-    this.blur.limit = 100
-
-    this.black = this.game.add.filter('Black')
-    this.black.setResolution(this.game.width, this.game.height)
-    this.black.limit = 150
-    this.black.centerx = this.game.width / 2
-    this.black.centery = this.game.height / 2
-
-    this.layer.filters = [this.blur, this.black]
+    this.createShader()
   },
 
   update: function () {
     this.game.physics.arcade.collide(this.player, this.layer)
 
     this.movePlayer()
-    var leftStickX = this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X)
-    var leftStickY = this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y)
-
-    if (!leftStickX) {
-      leftStickX = 0
-    }
-    if (!leftStickY) {
-      leftStickY = 0
-    }
-
-    this.player.body.velocity.x = leftStickX * 100
-    this.player.body.velocity.y = leftStickY * 100
-
-    if (leftStickX || leftStickY) {
-      this.player.rotation = Math.atan2(leftStickX, -leftStickY)
-    }
   },
 
   render: function () {
@@ -100,5 +63,47 @@ TheGame.prototype = {
   },
 
   movePlayer: function () {
+    var leftStickX = this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X)
+    var leftStickY = this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y)
+
+    if (!leftStickX) {
+      leftStickX = 0
+    }
+    if (!leftStickY) {
+      leftStickY = 0
+    }
+
+    console.log(leftStickX, leftStickY)
+
+    this.player.body.velocity.x = leftStickX * 100
+    this.player.body.velocity.y = leftStickY * 100
+
+    if (leftStickX || leftStickY) {
+      this.player.rotation = Math.atan2(leftStickX, -leftStickY)
+    }
+  },
+
+  createShader: function () {
+    this.blur = this.game.add.filter('MyBlur')
+    this.blur.blur = 20
+    this.blur.setResolution(this.game.width, this.game.height)
+    this.blur.centerx = this.game.width / 2
+    this.blur.centery = this.game.height / 2
+    this.blur.limit = this.view
+    this.black = this.game.add.filter('Black')
+    this.black.setResolution(this.game.width, this.game.height)
+    this.black.limit = this.light
+    this.black.centerx = this.game.width / 2
+    this.black.centery = this.game.height / 2
+    this.layer.filters = [this.blur, this.black]
+  },
+
+  createPlayer: function () {
+    this.start = this.findObject('start');
+    this.player = this.game.add.sprite(this.start.x, this.start.y, 'theman');
+    this.player.anchor.setTo(0.5, 0.5);
+    this.physics.enable(this.player, Phaser.Physics.ARCADE);
+
+    this.camera.follow(this.player)
   }
 }
